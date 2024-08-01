69527d7294a1ae872e3db77ea193da92fa0e66bc
Syed Azam Taji <muhammadazam030023@gmail.com>
2024-08-02 03:18:21 +0500

    Assignment 7 Done

---
+++ Assignment 7/Dart Assignment No 7/Q1.dart
@@ -0,0 +1,19 @@
+void main() {
+  print("Q1 use  any 10 List methods.\n");
+
+  List<String> Names = ["Ammar", "Arees", "Azam", "Moiz"];
+  print(Names.first); //Print First Index.
+  print(Names.hashCode); //Print HashCode.
+  print(Names.isEmpty); //Print False.
+  print(Names.isNotEmpty); //Print True.
+  print(Names.firstOrNull); //Print 1st Index or Null.
+  print(Names.lastOrNull); //Print Last Index or Null.
+  print(Names.last); //Print Last Index.
+  print(Names.length); //Print List Length.
+  print(Names.reversed); //Print Reversed The List.
+  print(Names.runtimeType); //Print List Type.
+  print(Names.indexed); //Print Index value Like(0,1,2,3).
+  print(Names.any((element) =>
+      element == "Arees")); //Element Present in List So Ans Is True.
+  print(Names.asMap()); //Print As A Map Structure.
+}
